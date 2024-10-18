543b5c23ac5bb4bf308580557caac2534762b7ed
shiyinxi <shiyinxi@gmail.com>
2024-10-18 11:48:17 -0700

    wave 01

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,10 @@
 from flask import Blueprint
+from app.models.planet import planets_dict
+
+planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
+
+@planet_bp.get("")
+def get_all_planets():
+    return planets_dict
+
 
