93bdda860de15e329014bf252b54579b42ea836e
Tatiana Trofimova <nerpassevera@gmail.com>
2024-10-25 13:18:20 -0700

    Adds routes

--- app/models/planet.py
+++ app/models/planet.py
@@ -15,51 +15,51 @@ class Planet(db.Model):
         }
 
 # planets_dicts = [
-#     {
-#         "id": 1, "name": "Mercury",
-#             "radius_in_mi": 1.516,
-#             "description": "about 1/3 the size of Earth"
-#         },
-#     {
-#         "id": 2,
-#         "name" : "Venus",
-#         "radius_in_mi" : 3.760,
-#         "description":  "only slightly smaller than Earth"
-#     },
-#     {
-#         "id": 3,
-#         "name" : "Earth",
-#         "radius_in_mi" : 3.959,
-#         "description": "Earth itself"
-#     },
-#     {
-#         "id": 4,
-#         "name" : "Mars",
-#         "radius_in_mi" : 2.106,
-#         "description":  "about half the size of Earth"
-#     },
-#     {
-#         "id": 5,
-#         "name" : "Jupiter",
-#         "radius_in_mi" : 43.441,
-#         "description": "11x Earth’s size"
-#     },
-#     {
-#         "id": 6,
-#         "name" : "Saturn",
-#         "radius_in_mi" : 36.184,
-#         "description": "9x larger than Earth"
-#     },
-#     {
-#         "id": 7,
-#         "name" : "Uranus",
-#         "radius_in_mi" : 15.759,
-#         "description":  "4x Earth’s size"
-#     },
-#     {
-#         "id": 8,
-#         "name" : "Neptune",
-#         "radius_in_mi" : 15.299,
-#         "description": "only slightly smaller than Uranus"
-#     }
+    # {
+    #     "id": 1, "name": "Mercury",
+    #         "radius_in_mi": 1.516,
+    #         "description": "about 1/3 the size of Earth"
+    #     },
+    # {
+    #     "id": 2,
+    #     "name" : "Venus",
+    #     "radius_in_mi" : 3.760,
+    #     "description":  "only slightly smaller than Earth"
+    # },
+    # {
+    #     "id": 3,
+    #     "name" : "Earth",
+    #     "radius_in_mi" : 3.959,
+    #     "description": "Earth itself"
+    # },
+    # {
+    #     "id": 4,
+    #     "name" : "Mars",
+    #     "radius_in_mi" : 2.106,
+    #     "description":  "about half the size of Earth"
+    # },
+    # {
+    #     "id": 5,
+    #     "name" : "Jupiter",
+    #     "radius_in_mi" : 43.441,
+    #     "description": "11x Earth’s size"
+    # },
+    # {
+    #     "id": 6,
+    #     "name" : "Saturn",
+    #     "radius_in_mi" : 36.184,
+    #     "description": "9x larger than Earth"
+    # },
+    # {
+    #     "id": 7,
+    #     "name" : "Uranus",
+    #     "radius_in_mi" : 15.759,
+    #     "description":  "4x Earth’s size"
+    # },
+    # {
+    #     "id": 8,
+    #     "name" : "Neptune",
+    #     "radius_in_mi" : 15.299,
+    #     "description": "only slightly smaller than Uranus"
+    # }
 #     ]
--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,28 @@
-from flask import Blueprint, make_response, abort
-# from app.models.planet import planets
+from flask import Blueprint, make_response, abort, request
+from app.models.planet import Planet
+from .db import db
 
 planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
 
-# @planet_bp.get("")
-# def get_all_planets():
-#     return [planet.to_dict() for planet in planets]
+@planet_bp.post("/", strict_slashes=False)
+def add_planet():
+    request_body = request.get_json()
+    name = request_body["name"]
+    description = request_body["description"]
+    radius_in_mi = request_body["radius_in_mi"]
 
-# @planet_bp.get("/<planet_id>")
+    new_planet = Planet(
+        name=name,
+        description=description,
+        radius_in_mi=radius_in_mi
+    )
+    db.session.add(new_planet)
+    db.session.commit()
 
-# def get_one_planet(planet_id):
-#     planet = validate_planet(planet_id)
+    return new_planet.to_dict(), 201
 
-#     return planet.to_dict()
-
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except:
-#         response = {"messange" : f"planet id {planet_id} is invalid"}
-#         abort(make_response(response, 400))
-
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
-    
-#     response = {"messange" : f"planet with id {planet_id} not found"}
-#     abort(make_response(response, 404))
+@planet_bp.get("")
+def get_all_planets():
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+    return [ planet.to_dict() for planet in planets]
