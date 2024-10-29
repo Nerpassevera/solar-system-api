2133e6dc71d910a1df398a2b61aeeca5c06a9a2e
Tatiana Trofimova <nerpassevera@gmail.com>
2024-10-29 10:55:50 -0700

    Adds get_one_planet and validate_planet funcs

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
 @planet_bp.post("/", strict_slashes=False)
 def add_planet():
     request_body = request.get_json()
-    name = request_body["name"]
+    name = request_body["name"],
     description = request_body["description"]
     radius_in_mi = request_body["radius_in_mi"]
 
@@ -26,3 +26,24 @@ def get_all_planets():
     query = db.select(Planet).order_by(Planet.id)
     planets = db.session.scalars(query)
     return [ planet.to_dict() for planet in planets]
+
+@planet_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return planet.to_dict()
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        response = {"message": f"Planet ID {planet_id} is invalid."}
+        abort(make_response(response, 400))
+
+    query = db.select(Planet).where(Planet.id == planet_id)
+    planet = db.session.scalar(query)
+
+    if not planet:
+        response = {"message": f"Planet with ID {planet_id} not found."}
+        abort(make_response(response, 404))
+
+    return planet
