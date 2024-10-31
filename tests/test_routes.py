c671da1397a401582b20604fef3a514bb4560749
shiyinxi <shiyinxi@gmail.com>
2024-10-31 12:56:59 -0700

    add get one planet no record test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,6 +7,15 @@ def test_get_all_planets_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
 
+def test_get_one_planet_with_no_records(client):
+    # Act
+    response = client.get("/planets/2")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {"message": "Planet with ID 2 not found."}
+
 def test_get_one_planet(client, two_saved_planets):
     # Act
     response = client.get("/planets/2")
